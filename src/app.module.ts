0501284aa7286d6f2efca3430479a7d23dc9a8eb
ykgautam09 <yateeshgautam1357@gmail.com>
2022-02-26 21:44:14 +0530

    project requirements and final setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,37 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { WeatherModule } from './weather/weather.module';
+import { AccountModule } from './account/account.module';
+import { User } from './user/entities/user.entity';
+import { Account } from './account/entities/account.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DEFAULT_DB,
+      entities: [User, Account],
+      synchronize: false,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      // sortSchema: true,
+    }),
+    UserModule,
+    WeatherModule,
+    AccountModule,
+  ],
   controllers: [],
   providers: [],
 })
